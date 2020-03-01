1f41df2f5368a5c7e54ac5e5de77dea54fb3985d
josheir <josheir@outlook.com>
2020-03-01 00:06:45 -0500

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,159 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <windows.h>
+int const SCREENWIDTH = 800;
+int const SCREENHEIGHT = 600; 
+ 
+int main()
+{
+	sf::Sprite marioSpriteL;
+	sf::Sprite marioSpriteR;
+	sf::Sprite structure1Sprite;
+	sf::Texture texture1;
+	sf::Texture texture2;
+	sf::Texture texture3;
+
+	float positionX = ((SCREENWIDTH / 2) - (76 / 2));
+	float positionY = (SCREENHEIGHT - 76);
+	float x = positionX;
+	float y = positionY;
+	float velocityY = -10.0f;
+	float velocityX = 4.0f;
+	float gravity = 0.5f;
+
+
+	if (!texture1.loadFromFile("mario_L.png"))
+	{
+		// error...
+	}
+	if (!texture2.loadFromFile("mario_R.png"))
+	{
+		// error...
+	}
+	if (!texture3.loadFromFile("structure1.png"))
+	{
+		// error...
+	}
+	marioSpriteL.setTexture(texture2);
+	marioSpriteR.setTexture(texture1);
+	structure1Sprite.setTexture(texture3);
+
+	
+	
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML");
+	marioSpriteR.setPosition((SCREENWIDTH/2) - (76/2), (SCREENHEIGHT- 76));
+	marioSpriteL.setPosition((SCREENWIDTH / 2) - (76 / 2), (SCREENHEIGHT - 76));
+	int isLeft = 0;
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			isLeft = 1;
+			//marioSpriteL.setTexture(texture1);
+			// left key is pressed: move our character
+			x = x - 1;
+			positionX--;
+			marioSpriteL.setPosition(x, y);
+			marioSpriteR.setPosition(x, y);
+			window.clear();
+			window.draw(marioSpriteR);
+			window.display();
+
+			
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			
+			isLeft = 0;
+			//marioSpriteR.setTexture(texture2);
+			// left key is pressed: move our character
+			x = x + 1;
+			positionX++;
+			marioSpriteR.setPosition(x, y);
+			marioSpriteL.setPosition(x, y);
+			window.clear();
+			window.draw(marioSpriteL);
+			window.display();
+			
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+		{
+			do {
+				if (isLeft == 1)
+				{
+					velocityX = -4;
+				}
+				else
+				{
+					velocityX = 4;
+				}
+				positionY += velocityY;
+				positionX += velocityX;
+				velocityY += gravity;
+				
+				if (isLeft == 1)
+				{   
+					if (positionY > (SCREENHEIGHT - 76))
+					{
+						positionY = (SCREENHEIGHT - 76);
+						y = positionY;
+						velocityY = -10;
+						velocityX = 4.0;
+						x = positionX;
+						break;
+					}
+					window.clear();
+					marioSpriteL.setPosition(positionX, positionY);
+					marioSpriteR.setPosition(positionX, positionY);
+					window.draw(marioSpriteR);
+					window.display();
+					Sleep(20);
+					
+				}
+				else
+				{
+					if (positionY > (SCREENHEIGHT - 76))
+					{
+						positionY = (SCREENHEIGHT - 76);
+						y = positionY;
+						velocityY = -10;
+						velocityX = 4.0;
+						x = positionX;
+						break;
+					}
+					window.clear();
+					marioSpriteL.setPosition(positionX, positionY);
+					marioSpriteR.setPosition(positionX, positionY);
+					window.draw(marioSpriteL);
+					window.display();
+					Sleep(20);
+					
+				}
+			} while (1);
+		}
+
+		window.clear();
+		if (isLeft == 0)
+		{
+			window.draw(marioSpriteL);
+		}
+		else
+		{
+			window.draw(marioSpriteR);
+		}
+		window.display();
+
+
+	}
+	
+}
